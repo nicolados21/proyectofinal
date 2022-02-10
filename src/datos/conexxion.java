91b9af60f483a21c113dde4669b8173806a4921c
dylan <dylan@192.168.200.12>
2022-02-10 12:18:55 -0500

    awdasdawdsdsadwasdwasdwadwaf

---
+++ src/datos/conexxion.java
@@ -0,0 +1,19 @@
+
+package datos;
+
+import java.sql.*;
+public class conexxion {
+    private static Conecction con=null;
+    static String db="";
+    static String url="";
+    static String user="";
+    static String sdriver="com.mysql.cj.jdbc.Driver";
+    static String sdriver1="com.mysql.jdbc.Driver";
+
+        public conexxion() {
+            
+        }
+        }
+    
+    
+
